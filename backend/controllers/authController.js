3c90512f6a247e1f72b35f464a0c04e598037e2e
Ajaz Nazir Bhat <bhattaijaz174@gmail.com>
2025-12-30 11:43:49 +0530

    Add getMe function to retrieve current user information

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -137,3 +137,12 @@ export const restrictTo = (...roles) => {
     next();
   };
 };
+
+export const getMe = (req, res) => {
+  res.status(200).json({
+    status: "success",
+    data: {
+      user: req.user,
+    },
+  });
+};
